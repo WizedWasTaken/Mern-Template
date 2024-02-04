9cb8e34dd2d8c0f7410d9f0e7cb797023ecf5481
WizedWasTaken <jegerfattig1@gmail.com>
2024-02-04 01:30:51 +0100

    Add dependencies dotenv and @types/node

--- frontend/src/routes/users/users.tsx
+++ frontend/src/routes/users/users.tsx
@@ -5,23 +5,28 @@ import { ErrorType, UserType } from "../../lib/definitions";
 
 function UsersPage() {
   const [users, setUsers] = useState<UserType[]>([]);
+  const apiUrl = import.meta.env.VITE_API_URL as string;
 
   useEffect(() => {
-    fetch("http://localhost:4000/users")
+    fetch(`${apiUrl}/users`)
       .then((response: Response) => response.json())
       .then((data: UserType[]) => {
+        console.log("Success:", data);
         setUsers(data);
       })
       .catch((error: ErrorType) => {
         console.error("Error:", error);
       });
-  }, []);
+  }, [apiUrl]);
 
   return (
     <div className="flex flex-col items-center justify-center h-full bg-gray-200">
       <h1 className="mb-6 text-5xl font-bold text-gray-700">Users</h1>
       {users.map((user) => (
-        <div key={user.id} className="p-6 mb-4 bg-white rounded shadow-md w-80">
+        <div
+          key={user.username}
+          className="p-6 mb-4 bg-white rounded shadow-md w-80"
+        >
           <h2 className="mb-2 text-xl font-semibold text-gray-700">
             {user.username}
           </h2>
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_API_URL: string;
+}
+
+interface ViteEnv {
+  VITE_API_URL: string;
+}
+
+export default function ViteEnv(): Plugin;
